d65a386d16faeb8f571538a15c79ee2cbe5571ff
dimuthuh28 <dimuthuharshamal@gmail.com>
2025-02-12 18:22:14 +0530

    Initial MERN Setup
    Done with the initial setup for Tekit Labs website.

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use("/api/users", userRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
