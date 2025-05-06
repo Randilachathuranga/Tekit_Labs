00ac1437660210258976975f935e10fcc29d30ae
Shenal Rayantha <101634665+shenal-rd203@users.noreply.github.com>
2025-05-06 15:16:06 +0530

    navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../assets/logo.png'; 
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">Tekit Labs</div>
+      <div className="logo"><img src={logo} alt="Tekit Labs Logo" /></div>
       <ul className="nav-links">
-        <li>Home</li>
-        <li>Services</li>
-        <li>Blog</li>
+        <li className="selected">Services</li>
         <li>About</li>
         <li>Contact</li>
+        <li>Blog</li>
       </ul>
+      <div className="book-btn">
+        <div className="book-text">Book A Meeting</div>
+      </div>
     </nav>
   );
 }
